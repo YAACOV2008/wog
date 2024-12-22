5d5a134192f66c5b1e9d23739f8b71bbf05f061e
YAACOV2008 <bisror2010@gmail.com>
2024-12-22 18:43:20 +0200

    Moved UP max_diff variable

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def start_play():
                  :return: None
                  :rtype:
                  """
+    max_diff = 5
     while True:
-        max_diff = 5
         menu = input("Please choose a game to play:\n1. Memory Game - a sequence of numbers will appear for 1 second"
               " and you have to guess it back.\n2. Guess Game - guess a number and see if you chose like the"
               " computer.\n3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n"
