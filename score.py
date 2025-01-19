525203aa7d87ce180680f90bd22d6cbb0edfc085
BISROR Jacob <bisror2010@gmail.com>
2025-01-19 20:34:32 +0200

    Added variable POINTS_OF_WINNING

--- score.py
+++ score.py
@@ -4,13 +4,14 @@ from utils import SCORES_FILE_NAME
 
 
 def add_score(difficulty):
+    POINTS_OF_WINNING = difficulty * 3 + 5
     while True:
         try:
             score_file = open(SCORES_FILE_NAME, mode="r")
             score = (score_file.readlines())
             print(f"Your current score is: {score[0]}")
             score_file = open(SCORES_FILE_NAME, mode="w")
-            new_score = int(score[0])+(difficulty * 3 + 5)
+            new_score = int(score[0])+POINTS_OF_WINNING
             print("Your\033[1m new\033[0m score is:" + '\033[1m'+str(new_score))
             score_file.write(str(new_score))
             break
