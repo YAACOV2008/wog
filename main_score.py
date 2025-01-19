33a48dc904a70fb556c8757f463a32cfcf227dc2
BISROR Jacob <bisror2010@gmail.com>
2025-01-19 20:33:48 +0200

    Main score py file for Docker

--- main_score.py
+++ main_score.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 
+from utils import BAD_RETURN_CODE, SCORES_FILE_NAME
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def score_server():
+    try:
+        score_file = open(SCORES_FILE_NAME, mode="r")
+        score = (score_file.readlines())
+        score = int(score[0])
+        return f"""<html>
+    <head>
+        <title>WOG: Scores</title>
+    </head>
+    <body>
+        <h1>The score is:</h1>
+        <div id="score">{score}</div>
+    </body>
+</html>"""
+    except FileNotFoundError:
+        return render_template(f'{BAD_RETURN_CODE}.html')
 
 
 if __name__ == '__main__':
     app.run()
-
